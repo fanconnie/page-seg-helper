f4b832c5ebddbef5e05c7d02796c696f0e38e47c
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-08-01 16:07:18 +0000

    Create README.

--- drawboxes.py
+++ drawboxes.py
@@ -3,4 +3,15 @@
 # Read UW-III .BOX file. Draw onto an image.
 # davep 24-Jan-2013
 
-i
+import sys
+import os
+import Image
+import ImageDraw
+#import numpy as np
+
+import zonebox
+from basename import get_basename
+
+def load_image( imgfilename ) : 
+    # load the image
+    img = Imag
