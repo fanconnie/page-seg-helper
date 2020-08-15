f7cf693c58bc9be23a647982bf874494f7e679f0
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-08-15 06:53:29 +0000

    Better help

--- drawxml.py
+++ drawxml.py
@@ -2,4 +2,16 @@
 
 # Parse an OCRopus zone output XML.
 # Draw zones onto an image.
-# davep 21-
+# davep 21-Jan-2013
+
+import sys
+import os
+import Image
+import ImageDraw
+
+import gtruthxml
+from basename import get_basename
+
+def load_image( imgfilename ) : 
+    img = Image.open(imgfilename)
+    i
