4388bb63c58ab0c1ccc4a32fe0724287409d6637
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-09-12 17:02:58 +0000

    Added a newline

--- gtruthxml.py
+++ gtruthxml.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
 
 # Parse an OCRopus output XML.
-# davep 
+# davep 09-Feb-2013
+
+import sys
+import os
+import xml.etree.ElementTree as ET
+
+import rects
+
+def load_xml( xmlfilename ) :
+    tree = ET.parse(xmlfilename)
+    root = tree.getroot()
+    return root
+
+# pret
