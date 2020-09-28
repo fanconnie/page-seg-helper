6905d1cd6858c85a3205bf506ea89e8812ee8b5f
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-09-28 11:31:56 +0000

    updated gif

--- gtruthxml.py
+++ gtruthxml.py
@@ -20,4 +20,7 @@ class Zone( object ):
     corner_one = {"row":0,"col":0}
     corner_two = {"row":0,"col":0}
 
-    
+    def __init__( self, upper_left, lower_right ) : 
+        # incoming fields are x,y order 
+        self.corner_one["row"] = upper_left[1]
+        self.corner_one["col"] = upper_
