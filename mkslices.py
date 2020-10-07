a3e42b353c3d1287afc29e13575ea27579651caa
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-10-07 11:47:48 +0000

    Changes made by Dzhenko Penev (dzhenko@gmail.com).

--- mkslices.py
+++ mkslices.py
@@ -1,3 +1,13 @@
 #!python
 
-# Load an image. Bre
+# Load an image. Break apart into multiple images of 1", 2", 3", 4", 6" strips.
+# davep 29-Jan-2013
+
+import sys
+import os
+import numpy as np
+import Image
+
+from basename import get_basename
+
+def make_slices( da
