2380ca14d4ec4bd7b36db7358890c26e2ecdffb5
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-11-07 13:35:51 +0000

    Update gradle plugin version

--- numcrunch.py
+++ numcrunch.py
@@ -3,4 +3,11 @@
 import numpy as np
 import itertools
 
-import dat
+import datfile
+
+def print_latex_table( outfilename, output_list ) :
+    prefix ="""
+    \\begin{tabular}{| l | l | l | l | }
+    \\hline
+    Name    &   Total Metrics & Zeros & \\% Non Zero \\\\
+ 
