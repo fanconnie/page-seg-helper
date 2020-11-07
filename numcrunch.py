3bcd9d59922cc450cdf3f5b35791510f8c3f6a4f
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-11-07 14:12:01 +0000

    updating HighCharts 3.0.4 -> 3.0.9

--- numcrunch.py
+++ numcrunch.py
@@ -10,4 +10,13 @@ def print_latex_table( outfilename, output_list ) :
     \\begin{tabular}{| l | l | l | l | }
     \\hline
     Name    &   Total Metrics & Zeros & \\% Non Zero \\\\
- 
+    \\hline"""
+
+    data = """
+    {name} & {count} & {zeros} & {percent:.2f} \\\\
+    \\hline"""
+
+    postfix = """
+    \\end{tabular}"""
+
+    outfile = open(outf
