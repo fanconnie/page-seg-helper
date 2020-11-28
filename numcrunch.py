b3ff2455aa7975b6af93949722377ab8f0a15f25
fanconnie <143478602+fanconnie@users.noreply.github.com>
2020-11-28 07:50:18 +0000

    initial check-in.

--- numcrunch.py
+++ numcrunch.py
@@ -37,4 +37,12 @@ def count_zeros(dataset,algorithm,stripsize) :
 
 #    zeros = np.where(metrics==0)
 
-    num_nonzero = np.count_nonzero(
+    num_nonzero = np.count_nonzero(metrics)
+    num_elements = len(metrics)
+
+#    print name, num_elements, num_nonzero
+
+#    print "num_nonzero=",num_nonzero
+#    print "num_elements=",len(metrics)
+
+    return (num_nonzero, num_elements, float(num_nonzero)/float(
