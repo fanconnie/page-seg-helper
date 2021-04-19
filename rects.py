db2fc86fb00d2cffb15934b2dc0a5ccbd4597d30
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-04-19 17:10:34 +0000

    Initial load.

--- rects.py
+++ rects.py
@@ -1,3 +1,18 @@
 #!python
 
-# Parse a UWIII ground truth Box file. Slice into 1",2",3"
+# Parse a UWIII ground truth Box file. Slice into 1",2",3",4",etc new ground
+# truth files.
+#
+# davep 01-Feb-2013
+
+import sys
+import os
+
+import zonebox
+
+class Point(object):
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+
+    def __re
