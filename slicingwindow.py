a9178743b700e45a1e9a562c8d5e9490d9824018
fanconnie <143478602+fanconnie@users.noreply.github.com>
2021-07-11 06:17:15 +0000

    lf

--- slicingwindow.py
+++ slicingwindow.py
@@ -1,4 +1,15 @@
 #!python
 
 # Slice a window of N rows down an image by M rows (the slide) each step.
-# Write each
+# Write each strip to an individual image file in a subdirectory.
+#
+# created for the page segmentation comparisson
+#
+# davep 5-Feb-2013
+
+import sys
+import numpy as np
+import Image
+import os
+
+import mks
