121356ee3d20c7f9443d37ade949ae00a51be82a
fanconnie <143478602+fanconnie@users.noreply.github.com>
2022-07-09 15:28:14 +0000

    Fixed again

--- slicingwindow.py
+++ slicingwindow.py
@@ -159,4 +159,9 @@ def make_sliding_strips_from_image( imgfilename, output_dir ) :
 
     # get the image as a numpy array
     data = mkslices.load_image( imgfilename )
-    strip_l
+    strip_list = make_all_strips_images( data, basename, output_dir )
+
+def awinder( ) : 
+    # slice up Amy Winder's images 
+    output_dir_base = "{0}_winder/".format(num_rows_in_strip)
+#  
